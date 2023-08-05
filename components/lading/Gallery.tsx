f8a420caee10a7c4d560ddead1a9e58073ec283a
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-08-04 22:48:16 -0600

    Galeria terminada refactorizar codigo

--- components/lading/Gallery.tsx
+++ components/lading/Gallery.tsx
@@ -9,14 +9,14 @@ import { DecoratedTitle } from '../ui';
 export const Gallery = () => {
   return (
     <>
-      <Grid item xs={12} md={6}>
+      <Grid item xs={12} lg={6}>
 
         <DecoratedTitle title='Galeria'>
 
         </DecoratedTitle>
       </Grid>
 
-      <Box sx={{ width: '100%', position: 'relative', display: { xs: 'flex', 'md': 'none'}, overflowX: 'scroll', scrollSnapType: 'x'}}>
+      <Box sx={{ width: '100%', position: 'relative', display: { xs: 'flex', md: 'none'}, overflowX: 'scroll', scrollSnapType: 'x'}}>
         {
           dataToTest.map(({img, desc}, idx) => (
             <CardContent
@@ -24,22 +24,53 @@ export const Gallery = () => {
               component='img'
               src={img}
               alt={desc}
-              sx={{ width: '100%', left: 0, position: 'sticky', objectFit: 'cover', scrollSnapAlign: 'center'}}
+              sx={{ width: 300, height: 300, left: 0, position: 'sticky', objectFit: 'cover', scrollSnapAlign: 'center'}}
             />
           ))
         }
       </Box>
 
-      <Grid item md={6}>
+      <Grid item lg={6} sx={{ display: {xs: 'none', md: 'block'}}}>
         <Grid container sx={{ p: 2}}>
           <Grid item md={6}>
+            {
+              dataToTest.map(( (data, idx) => {
+                
+                  return ( idx < (dataToTest.length / 2) ) 
+                    ? (
+                      <CardContent
+                        key={idx}
+                        component='img'
+                        src={data.img}
+                        alt={data.desc}
+                        sx={{ width: '100%', mt: 1, borderRadius: '20px'}}
+                      />
+                    )
+                    : <></>
+              }))
+            }
           </Grid>
+
           <Grid item md={6}>
+            {
+              dataToTest.map(( (data, idx) => {
+                
+                return ( idx >= (dataToTest.length / 2) ) 
+                  ? (
+                    <CardContent
+                      key={idx}
+                      component='img'
+                      src={data.img}
+                      alt={data.desc}
+                      sx={{ width: '100%', mt: idx === 4 ? 10 : 1,  borderRadius: '20px'}}
+                    />
+                  )
+                  : <></>
+              }))
+            }
           </Grid>
         </Grid>
       </Grid>
-    
-
     </>
   )
 }
--- components/lading/InfoView.tsx
+++ components/lading/InfoView.tsx
@@ -22,7 +22,7 @@ const stylesContainer: SxProps<Theme> = {
 export const InfoCards = () => {
   return (
     <>
-      <Grid item xs={12} lg={3} sx={stylesContainer}>
+      <Grid item xs={12} md={3} sx={stylesContainer}>
           <CardMedia
             component={'img'}
             alt='Personas tatuadas'
@@ -37,7 +37,7 @@ export const InfoCards = () => {
           />
       </Grid>
 
-      <Grid item xs={12} lg={3} sx={stylesContainer}>
+      <Grid item xs={12} md={3} sx={stylesContainer}>
         <CardMedia
           component={'img'}
           alt='Personas tatuadas'
@@ -52,7 +52,7 @@ export const InfoCards = () => {
         />
       </Grid>
 
-      <Grid item xs={12} lg={6} >
+      <Grid item xs={12} md={6} >
         <DecoratedTitle title='Acerca de nosotros'>
           <Typography variant='body1'>
             In aute duis duis tempor ad magna incididunt dolor eiusmod. Cupidatat ut minim laborum do nisi mollit ea in qui enim cupidatat id officia. Do mollit culpa commodo sunt ipsum. Elit eiusmod ullamco esse minim qui amet ea elit incididunt ut culpa ut minim reprehenderit. Excepteur elit esse nostrud nostrud enim et. Cupidatat minim enim exercitation esse laboris amet consectetur do officia cillum pariatur dolor ad officia. Laborum fugiat labore excepteur esse voluptate aliquip.
